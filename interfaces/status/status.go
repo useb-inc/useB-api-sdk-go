5b9ef1fd9662138f7242bedd82c7146031c4a0ad
WonJun Choi <jack@useb.co.kr>
2022-11-28 13:46:37 +0900

    add: status API

--- interfaces/status/status.go
+++ interfaces/status/status.go
@@ -12,3 +12,71 @@ type IdcardResponse struct {
 	Message       string `json:"message"`
 	TransactionId string `json:"transaction_id"`
 }
+
+type DriverRequest struct {
+	UserName  string `validate:"required" json:"userName"`
+	BirthDate string `validate:"required" json:"birthDate"`
+	LicenseNo string `validate:"required" json:"licenseNo"`
+	JuminNo   string `json:"juminNo"`
+	IsSecret  bool   `json:"secret_mode"`
+}
+type DriverResponse struct {
+	Success       bool   `json:"success"`
+	Message       string `json:"message"`
+	TransactionId string `json:"transaction_id"`
+}
+
+type PassportRequest struct {
+	UserName       string `validate:"required" json:"userName"`
+	BirthDate      string `validate:"required" json:"birthDate"`
+	PassportNo     string `validate:"required" json:"passportNo"`
+	IssueDate      string `validate:"required" json:"issueDate"`
+	ExpirationDate string `validate:"required" json:"expirationDate"`
+	IsSecret       bool   `json:"secret_mode"`
+}
+type PassportResponse struct {
+	Success       bool   `json:"success"`
+	Message       string `json:"message"`
+	TransactionId string `json:"transaction_id"`
+}
+
+type PassportOverseasRequest struct {
+	PassportNo  string `validate:"required" json:"passportNo"`
+	Nationality string `validate:"required" json:"nationality"`
+	BirthDate   string `validate:"required" json:"birthDate"`
+	IsSecret    bool   `json:"secret_mode"`
+}
+type PassportOverseasResponse struct {
+	Success       bool   `json:"success"`
+	Message       string `json:"message"`
+	TransactionId string `json:"transaction_id"`
+}
+
+type AlienRequest struct {
+	IssueNo   string `validate:"required" json:"issueNo"`
+	IssueDate string `validate:"required" json:"issueDate"`
+	IsSecret  bool   `json:"secret_mode"`
+}
+type AlienResponse struct {
+	Success       bool   `json:"success"`
+	Message       string `json:"message"`
+	TransactionId string `json:"transaction_id"`
+}
+
+type BusinessRegistrationRequest struct {
+	BizNo string `validate:"required" json:"biz_no"`
+}
+
+type BusinessRegistrationData struct {
+	TaxTypeCode       string `json:"tax_type_code"`
+	TaxTypeName       string `json:"tax_type_name"`
+	ClosingDate       string `json:"closing_date"`
+	TaxTypeChangeDate string `json:"tax_type_change_date"`
+}
+
+type BusinessRegistrationResponse struct {
+	Success       bool                      `json:"success"`
+	Message       string                    `json:"message"`
+	TransactionId string                    `json:"transaction_id"`
+	Data          *BusinessRegistrationData `json:"data,omitempty"`
+}
--- resources/status.go
+++ resources/status.go
@@ -7,11 +7,21 @@ import (
 )
 
 const (
-	URLIdcard string = "/status/idcard"
+	URLIdcard               string = "/status/idcard"
+	URLDriver               string = "/status/driver"
+	URLPassport             string = "/status/passport"
+	URLPassportOverseas     string = "/status/passport-overseas"
+	URLAlien                string = "/status/alien"
+	URLBusinessRegistration string = "/status/business-registration"
 )
 
 type Status struct {
-	Idcard func(*status.IdcardRequest) (*status.IdcardResponse, error)
+	Idcard               func(*status.IdcardRequest) (*status.IdcardResponse, error)
+	Driver               func(*status.DriverRequest) (*status.DriverResponse, error)
+	Passport             func(*status.PassportRequest) (*status.PassportResponse, error)
+	PassportOverseas     func(*status.PassportOverseasRequest) (*status.PassportOverseasResponse, error)
+	Alien                func(*status.AlienRequest) (*status.AlienResponse, error)
+	BusinessRegistration func(*status.BusinessRegistrationRequest) (*status.BusinessRegistrationResponse, error)
 }
 
 func NewStatus(auth *Auth) *Status {
@@ -19,8 +29,18 @@ func NewStatus(auth *Auth) *Status {
 		Host: API_BASE_HOST,
 	}
 	idcard := callAPIMethod[status.IdcardRequest, status.IdcardResponse](base, auth, http.MethodPost, URLIdcard)
+	driver := callAPIMethod[status.DriverRequest, status.DriverResponse](base, auth, http.MethodPost, URLDriver)
+	passport := callAPIMethod[status.PassportRequest, status.PassportResponse](base, auth, http.MethodPost, URLPassport)
+	passportOverseas := callAPIMethod[status.PassportOverseasRequest, status.PassportOverseasResponse](base, auth, http.MethodPost, URLPassportOverseas)
+	alien := callAPIMethod[status.AlienRequest, status.AlienResponse](base, auth, http.MethodPost, URLAlien)
+	businessRegistration := callAPIMethod[status.BusinessRegistrationRequest, status.BusinessRegistrationResponse](base, auth, http.MethodPost, URLBusinessRegistration)
 
 	return &Status{
-		Idcard: idcard,
+		Idcard:               idcard,
+		Driver:               driver,
+		Passport:             passport,
+		PassportOverseas:     passportOverseas,
+		Alien:                alien,
+		BusinessRegistration: businessRegistration,
 	}
 }
